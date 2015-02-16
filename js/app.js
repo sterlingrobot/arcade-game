ffaff0411a9d5e7acf5eb2f26a05da2876f16213
Tor Gilbertson <sterlingrobot@gmail.com>
2015-02-15 23:09:01 -0700

    Initial render and placement of player, enemies

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = Math.ceil(Math.random() * 3) * 83 - 83 * 0.3;
 }
 
 // Update the enemy's position, required method for game
@@ -24,13 +26,54 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 2 * 101;
+    this.y = 5 * 83 - 41.5;
+}
 
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(dir) {
+    switch(dir) {
+        case 'left':
+            if(this.x - 101 < 0) return;
+            this.x -= 101;
+        break;
+        case 'up':
+            if(this.y - 83 < 0) return;
+            this.y -= 83;
+        break;
+        case 'right':
+            if(this.x + 101 > 505 - 101) return;
+            this.x += 101;
+        break;
+        case 'down':
+            if(this.y + 83 > 606 - (83 * 2)) return;
+            this.y += 83;
+        break;
+        default:
+        break;
+    }
+
+    this.render();
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+for(var i = 0; i < Math.ceil(Math.random()*10); i++) {
+    allEnemies.push(new Enemy());
+}
 
-
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
